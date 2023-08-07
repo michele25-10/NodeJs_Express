7ccea5710e0ced03ffc23656bc81e595b34a98bc
michele25-10 <michele.gabri01@gmail.com>
2023-08-07 18:15:34 +0200

    AsyncHandler
    Migliorato l'errorHandler con l'aggiunta di nuovi errori.
    Aggiunto anche il modulo node js di express-asyn-handler

--- express-project/mycontacts-backend/controllers/contactController.js
+++ express-project/mycontacts-backend/controllers/contactController.js
@@ -5,17 +5,21 @@ const getContacts = (req, res) => {
     res.status(200).json({ message: "Ottieni tutti i contatti" });
 }
 
+const asyncHandler = require('express-async-handler');
+/*Con la funzione asincrona non ci sarà la necessità di un tryCatch block, il modulo 
+di nodeJs asyncHandler si occuperà in modo autonomo di rilevare l'errore e passarlo all'errorHandler*/
+
 //@desc Get contact
 //@route GET /api/contacts/:id
 //@access public
-const getContact = (req, res) => {
+const getContact = asyncHandler(async (req, res) => {
     res.status(200).json({ message: 'Ottieni il contatto' + req.params.id });
-}
+});
 
 //@desc Post contact
 //@route POST /api/contacts
 //@access public
-const createContact = (req, res) => {
+const createContact = asyncHandler(async (req, res) => {
     console.log(req.body);
     const { name, email, phone } = req.body;
     if (!name || !email || !phone) {
@@ -23,20 +27,20 @@ const createContact = (req, res) => {
         throw new Error("Tutti i dati sono obbligatori");
     }
     res.status(201).json("The request body is: " + req.body);
-}
+});
 
 //@desc Update contact
 //@route Put /api/contacts/:id
 //@access public
-const updateContact = (req, res) => {
+const updateContact = asyncHandler(async (req, res) => {
     res.status(200).json({ message: 'Modifica il contatto' + req.params.id });
-}
+});
 
 //@desc Delete contact
 //@route delete /api/contacts/:id
 //@access public
-const deleteContact = (req, res) => {
+const deleteContact = asyncHandler(async (req, res) => {
     res.status(200).json({ message: 'Elimina il contatto' + req.params.id });
-}
+});
 
 module.exports = { getContacts, getContact, createContact, updateContact, deleteContact };
