62d13fde279f06495100ad6a84571cb1bfb95534
michele25-10 <michele.gabri01@gmail.com>
2023-08-08 11:58:32 +0200

    Installazione di bcrypt ed api register funzionante

--- express-project/mycontacts-backend/controllers/userController.js
+++ express-project/mycontacts-backend/controllers/userController.js
@@ -1,10 +1,32 @@
 const asyncHandler = require("express-async-handler");
-
+const bcrypt = require("bcrypt");
+const User = require("../models/userModel");
 //@desc registrare un utente
 //@route POST /api/users/register
 //@access public
 const registerUser = asyncHandler(async (req, res) => {
+    const { username, email, password } = req.body;
+    if (!username || !email || !password) {
+        res.status(400);
+        throw new Error("Tutti i campi sono obbligatori");
+    }
 
+    const userAvailable = await User.findOne({ email: email }).exec();
+    if (userAvailable) {
+        res.status(400);
+        throw new Error("Utente già esistente");
+    }
+
+    //Hash password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({ username, email, password: hashedPassword });
+    if (user) {
+        res.status(201).json({ _id: user.id, email: user.email });
+    } else {
+        res.status(404);
+        throw new Error("Registrazione fallita");
+    }
 });
 
 //@desc loggare un utente
