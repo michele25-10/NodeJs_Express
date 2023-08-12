70db3f94fcaa7d2d29b9489d3bca713c4500e182
michele25-10 <michele.gabri01@gmail.com>
2023-08-12 17:48:03 +0200

    Promise e variabili

---
+++ variabili.js
@@ -0,0 +1,11 @@
+undefined;  // non è stata inizializzata e non ha valori
+null; // Ha valore nullo
+boolean; // valore booleano o true o false
+object; //oggetto
+
+var ilMioNome = 'Michele';  //variabile che ha un valore in tutto il programma
+ilMioNome = 10;
+
+let età = 19;   //Ha valore solo nell'ambiente in cui viene creata
+
+const piGreco = 3.14; // ha valore costante non può essere modificato
